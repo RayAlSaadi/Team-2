7f32580abef15d13024780e924db2992f34755de
kjs29-37 <kabirsarham@icloud.com>
2024-11-01 11:51:33 +0000

    DB connection
    testing

---
+++ kjs.php
@@ -0,0 +1,10 @@
+$dbname = "railway_ticket_booking";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
